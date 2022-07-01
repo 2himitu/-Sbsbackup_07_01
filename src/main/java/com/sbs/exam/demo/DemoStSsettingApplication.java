0be1b78b75473f965201dcff02b913553afbf98e
2himitu <hmhnes@gmail.com>
2022-07-02 00:01:46 +0900

    스프링 세팅 07-01

---
+++ src/main/java/com/sbs/exam/demo/DemoStSsettingApplication.java
@@ -0,0 +1,13 @@
+package com.sbs.exam.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoStSsettingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoStSsettingApplication.class, args);
+	}
+
+}
