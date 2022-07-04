d7c1a1c38ad670ba42e69f813fe8cb039222c935
2himitu <hmhnes@gmail.com>
2022-07-04 19:09:08 +0900

    browserType test

--- src/main/java/com/sbs/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/sbs/exam/demo/controller/UsrHomeController.java
@@ -1,8 +1,15 @@
 package com.sbs.exam.demo.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import lombok.Data;
 
 @Controller
 public class UsrHomeController {
@@ -11,19 +18,67 @@ public class UsrHomeController {
 	public UsrHomeController() {
 		count = 0; 
 	}
-
-	@RequestMapping ("/usr/home/getCount")
+	@RequestMapping ("usr/home/getString")
 	@ResponseBody
-	public int getCount() {
-		return count;
-		
+	public String getString() {
+		return "String";
 	}
-	@RequestMapping ("/usr/home/doSetCount")
+	@RequestMapping ("usr/home/getInt")
 	@ResponseBody
-	public String doSetCount(int count) {
-		this.count = count ;
-		return "count 값을"+this.count+"로 초기화 하였습니다.";
+	public int getInteger() {
+		return 1;
+	}
+	@RequestMapping ("usr/home/getDouble")
+	@ResponseBody
+	public double getDouble() {
+		return 15.2;
+	}
+	@RequestMapping ("usr/home/getFloat")
+	@ResponseBody
+	public float getFloat() {
+		return 10.3f;
+	}
+	@RequestMapping ("usr/home/getBoolean")
+	@ResponseBody
+	public boolean getBoolean() {
+		return false;
+	}
+	@RequestMapping ("usr/home/getCharacter")
+	@ResponseBody
+	public char getCharacter() {
+		return 'a';
+	}
+	@RequestMapping ("usr/home/getMap")
+	@ResponseBody
+	public Map<String, Object> getMap() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("철수", 22);
+		map.put("영희", 21);
+		return map;
 	}
 	
-
+	@RequestMapping ("usr/home/getList")
+	@ResponseBody
+	public List<String> getList() {
+		List<String> list = new ArrayList<>();
+		list.add("철수");
+		return list;
+	}
+	@RequestMapping ("usr/home/getArticle")
+	@ResponseBody
+	public Article getArticle() {
+		Article article = new Article();
+		return article;
+	}
+	
+}
+@Data
+class Article {
+	private int id;
+	private String title;
+	
+	public Article() {
+		id = 1;
+		title = "제목";
+	}
 }
