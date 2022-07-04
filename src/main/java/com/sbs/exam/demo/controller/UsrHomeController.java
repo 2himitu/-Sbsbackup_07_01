d044bf5818d7ebc0b58f8bf2f9045e4326007599
2himitu <hmhnes@gmail.com>
2022-07-04 18:22:08 +0900

    getCount&doSetCount Action +

--- src/main/java/com/sbs/exam/demo/controller/UsrHomeController.java
+++ src/main/java/com/sbs/exam/demo/controller/UsrHomeController.java
@@ -11,33 +11,18 @@ public class UsrHomeController {
 	public UsrHomeController() {
 		count = 0; 
 	}
-	
-	@RequestMapping ("/usr/home/main")
+
+	@RequestMapping ("/usr/home/getCount")
 	@ResponseBody
-	public String showMain() {
-		return "안녕하세요.";
-	}
-	@RequestMapping ("/usr/home/main2")
-	@ResponseBody
-	public String showMain2() {
-		return "반갑습니다.";
-	}
-	@RequestMapping ("/usr/home/main3")
-	@ResponseBody
-	public String showMain3() {
-		return "또 만나요.";
-	}
-	@RequestMapping ("/usr/home/main4")
-	@ResponseBody
-	public int showMain4() {
-		return count++;
+	public int getCount() {
+		return count;
 		
 	}
-	@RequestMapping ("/usr/home/main5")
+	@RequestMapping ("/usr/home/doSetCount")
 	@ResponseBody
-	public String showMain5() {
-		count = 0 ;
-		return "count 값을 초기화 하였습니다.";
+	public String doSetCount(int count) {
+		this.count = count ;
+		return "count 값을"+this.count+"로 초기화 하였습니다.";
 	}
 	
 
