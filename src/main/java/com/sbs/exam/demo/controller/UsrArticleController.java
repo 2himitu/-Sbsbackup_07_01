df9e2c2add5b4d9ac00ec3a0cc6e07c5780223d2
2himitu <hmhnes@gmail.com>
2022-07-05 19:46:26 +0900

    writeArticle추가

--- src/main/java/com/sbs/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/sbs/exam/demo/controller/UsrArticleController.java
@@ -24,19 +24,28 @@ public class UsrArticleController {
 	
 	private void makeTestData() {
 		for(int i = 1;i <= 10;i++) {
-			Article article = new Article(i,"제목"+i,"내용"+i);
-			articles.add(article);
+			String title = "제목"+i;
+			String body = "내용"+i;
+			writeArticle(title,body);
 		}
 		
 	}
+	public Article writeArticle(String title ,String body) {
+		int id = articleLastId+1;
+		Article article = new Article(id,title,body);
+		
+		articles.add(article);
+		articleLastId = id;
+		
+		return article;
+	}
+	
 
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title ,String body) {
-		int id = articleLastId+1;		
-		Article article = new Article(id,title,body);
-		articles.add(article);
-		articleLastId =id;
+		Article article = writeArticle(title ,body);
+		
 		return article;
 	}
 	@RequestMapping("/usr/article/getArticles")
@@ -48,12 +57,7 @@ public class UsrArticleController {
 	@RequestMapping("usr/article/doDelete")
 	@ResponseBody
 	public String doDelete(int id){
-//		for(int i = 0 ; i < articles.size();i++) {
-//			if(articles.get(i).getId()==id) {
-//				articles.remove(i);
-//				break;
-//			}
-//		}
+
 		articles.remove(id);
 		return id+"번 째 게시물을 삭제 하였습니다.";
 	}
