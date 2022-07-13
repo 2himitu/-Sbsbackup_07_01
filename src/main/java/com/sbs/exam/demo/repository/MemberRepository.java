405211a89506fad74cfc7680785b4eb5c09a1ed1
2himitu <hmhnes@gmail.com>
2022-07-13 19:53:37 +0900

    UsrMemberController,MemberService 추가

---
+++ src/main/java/com/sbs/exam/demo/repository/MemberRepository.java
@@ -0,0 +1,8 @@
+package com.sbs.exam.demo.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MemberRepository {
+
+}
---
+++ src/main/java/com/sbs/exam/demo/vo/Member.java
@@ -0,0 +1,19 @@
+package com.sbs.exam.demo.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+	String loginId;
+	String loginPw;
+	String name;
+	String nickname;
+	String cellphoneNo;
+	String email;
+	
+
+}
