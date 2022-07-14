68b62de90884de8d3206a2883e47d6160d572bdf
2himitu <hmhnes@gmail.com>
2022-07-14 11:54:18 +0900

    MemberRepository 추가, 회원가입 처리

--- src/main/java/com/sbs/exam/demo/service/MemberService.java
+++ src/main/java/com/sbs/exam/demo/service/MemberService.java
@@ -2,15 +2,18 @@ package com.sbs.exam.demo.service;
 
 import org.springframework.stereotype.Service;
 
-//import com.sbs.exam.demo.repository.MemberRepository;
+import com.sbs.exam.demo.repository.MemberRepository;
 
 @Service
 public class MemberService {
-
-	//MemberRepository memberRepository;
-	public static void join(String loginId, String loginPw, String name, String nickname, String cellphoneNo,
+	private MemberRepository memberRepository;
+	public MemberService(MemberRepository memberRepository) {
+		this.memberRepository=memberRepository;
+	}
+	
+	public void join(String loginId, String loginPw, String name, String nickname, String cellphoneNo,
 			String email) {
-		//memberRepository.join(loginId,loginPw,name,nickname,cellphoneNo,email);
+		memberRepository.join(loginId,loginPw,name,nickname,cellphoneNo,email);
 		
 		
 	}
