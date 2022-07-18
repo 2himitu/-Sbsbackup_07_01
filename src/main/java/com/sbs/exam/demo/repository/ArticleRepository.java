38d646a901130650bdec73b744a399fdd7eaf5b7
2himitu <hmhnes@gmail.com>
2022-07-18 21:52:37 +0900

    상세페이지에서 삭제,출력용 게시물 가져올 때, 액터가 해당 게시물을 삭제/수정 할 수 있는지도 파악, 수정 버튼 추가,Rq 클래스 도입으로 로그인 여부와 관련된 중복된 로직 제거,BeforeAction 인터셉터 구현

--- src/main/java/com/sbs/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/sbs/exam/demo/repository/ArticleRepository.java
@@ -4,15 +4,33 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import com.sbs.exam.demo.vo.Article;
 
 @Mapper
 public interface ArticleRepository {
 	public void writeArticle(@Param("memberId") int memberId, @Param("title") String title, @Param("body") String body);
+	
+	
+	@Select("""
+			SELECT A.* ,
+			M.nickname AS extra__writerName
+			FROM article AS A
+			LEFT JOIN member AS M
+			ON A.memberId = M.id
+			WHERE 1
+			AND A.id = #{id}
+			""")
+	public Article getForPrintArticle(@Param("id") int id);
 
-	public Article getArticle(@Param("id") int id);
-
+	@Select("""
+			SELECT A.* ,
+			M.nickname AS extra__writerName
+			FROM article AS A
+			LEFT JOIN member AS M
+			ON A.memberId = M.id
+			""")
 	public List<Article> getArticles();
 
 	public void deleteArticle(@Param("id") int id);
