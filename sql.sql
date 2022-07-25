9a7e4208ae494b39188251a8dd69bdb2b9a253d4
2himitu <hmhnes@gmail.com>
2022-07-25 21:41:54 +0900

    게시물 늘리기 ,게시물 개수 표시

--- sql.sql
+++ sql.sql
@@ -30,6 +30,7 @@ title = '제목 3',
 
 SELECT * FROM article;
 
+#맴버 테이브 생성
 CREATE TABLE `member` (
 id INT (10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
 regDate DATETIME NOT NULL,
@@ -43,7 +44,6 @@ cellphoneNo CHAR(20) NOT NULL,
 email CHAR(50) NOT NULL,
 delStatus TINYINT (1) UNSIGNED NOT NULL  DEFAULT 0 COMMENT '탈퇴여부(0=탈퇴전 ,1=탈퇴)',
 delDate DATETIME COMMENT'탈퇴날짜'
-
 );
 
 INSERT INTO `member`
@@ -96,6 +96,56 @@ UPDATE article
 SET memberId = 2
 WHERE memberId = 0;
 
+#게시판 테이블 생성
+CREATE TABLE Board(
+id INT (10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+regDate DATETIME NOT NULL,
+updateDate DATETIME NOT NULL,
+`code` CHAR(50) NOT NULL UNIQUE COMMENT 'notice (공지사항),free1(자유게시판1),free2(자유게시판2)..',
+`name` CHAR(50) NOT NULL UNIQUE COMMENT '게시판 이름',
+delStatus TINYINT (1) UNSIGNED NOT NULL  DEFAULT 0 COMMENT '삭제여부(0=탈퇴전 ,1=탈퇴)',
+delDate DATETIME COMMENT'삭제날짜'
+);
+
+DESC BOARD;
+
+#게시판 생성
+INSERT INTO board
+SET regDate = NOW(),
+updateDate = NOW(),
+`code` = 'notice',
+`name` = '공지사항';
+
+INSERT INTO board
+SET regDate = NOW(),
+updateDate = NOW(),
+`code` = 'free1',
+`name` = '자유';
+
+# 게시판 테이블에 boardId 칼럼 추가
+ALTER TABLE article ADD COLUMN boardId INT (10) UNSIGNED NOT NULL AFTER memberId;
+
+
+# 게시판에 강제로 게시판 정보 넣기
+UPDATE article
+SET boardId =1
+WHERE id IN (1,2);
+
+UPDATE article
+SET boardId =2
+WHERE id IN (3);
+
 SELECT * FROM article;
 
+SELECT * FROM board WHERE id = 1;
 
+# 게시물 늘기기
+ INSERT INTO article 
+ (
+ regDate,updateDate,memberId,boardId,title,`body`
+ )
+ SELECT NOW(),NOW(),FLOOR(RAND() * 2 ) + 1,FLOOR(RAND() * 2 ) + 1,
+ CONCAT('제목_',RAND()),CONCAT('내용_',RAND()) 
+ FROM article;
+
+SELECT COUNT(*) FROM article;
